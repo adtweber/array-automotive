13f6b884397cda8dc9f752a991f8e3130f6e9939
Andi Weber <adtweber@gmail.com>
2024-03-24 10:25:11 -0400

    added loadpassenger method

--- mercurySedan.js
+++ mercurySedan.js
@@ -5,7 +5,7 @@ const VehicleModule = require("./vehicleBaseClass")
 let v = new VehicleModule.Vehicle("Mercury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
 
-class Car extends vehicleModule {
+class Car extends VehicleModule.Vehicle {
     constructor(make, model, year, color, mileage) {
         super(make, model, year, color, mileage);
         this.maxPassengers = 5;
@@ -16,6 +16,25 @@ class Car extends vehicleModule {
         this.scheduledService = false;
     }
 
+    loadPassenger(num) {
+        if (this.passenger < this.maxPassengers) {
+            if ((num + this.passenger) <= this.maxPassengers) {
+                this.passenger = num;
+                return this.passenger;               
+            } else {
+                console.log(this.model + " " + this.make + " not have enough space to take all passengers.");
+
+            }
+        } else {
+            console.log(this.model + " " + this.make + " is full");
+        }
+    }
+
+    checkService() {
+        if (this.mileage > 30000)
+            this.scheduleService = true
+            return this.scheduleService;
+    }
 }
 //After you write the derived Car class, you should test it out.
 
